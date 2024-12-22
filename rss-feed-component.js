d3d98015d1c974f61489bb7c35263160d3594980
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2024-12-22 16:36:53 +1100

    feat: add result-pagination-info component and rss-feed-last-updated component
    result-pagination-info: displays the information about the current set of results and allows navigation between pages.
    rss-feed-last-updated: displays the last updated timestamp of the feed.

---
+++ rss-feed-component.js
@@ -0,0 +1,4 @@
+const rssFeedUrl = document.querySelector(".events-list").dataset.rssUrl;
+const cardLimit = document.querySelector(".events-list").dataset.cardLimit;
+console.log(rssFeedUrl);
+console.log(cardLimit);
