c009b06c5bdfc7181f70e81c1922241bd4b24a89
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2024-12-23 04:06:42 +1100

    feat: add comments to the code in rss-feed-component.js file

--- rss-feed-component.js
+++ rss-feed-component.js
@@ -1,3 +1,9 @@
+/**
+ * Converts a 12-hour time format string to a 24-hour time format.
+ *
+ * @param {string} timeString - The time string in 12-hour format (e.g., "02:30 PM").
+ * @returns {{hours: number, minutes: number}} An object containing the hours and minutes in 24-hour format.
+ */
 const convertTo24HourFormat = (timeString) => {
 	const [time, modifier] = timeString.split(" ");
 	let [hours, minutes] = time.split(":").map(Number);
@@ -8,6 +14,14 @@ const convertTo24HourFormat = (timeString) => {
 	}
 	return { hours, minutes };
 };
+
+/**
+ * Calculates the duration in minutes between two given times in 24-hour format.
+ *
+ * @param {Object} startTimeIn24HourFormat - The start time in 24-hour format.
+ * @param {Object} endTimeIn24HourFormat - The end time in 24-hour format.
+ * @returns {number} The duration in minutes between the start and end times.
+ */
 const calculateDurationInMinutes = (
 	startTimeIn24HourFormat,
 	endTimeIn24HourFormat
@@ -22,6 +36,14 @@ const calculateDurationInMinutes = (
 	}
 	return durationInMinutes;
 };
+
+/**
+ * Converts a JavaScript Date object to a Luxon DateTime object with the specified time zone.
+ *
+ * @param {Date} originalDateTime - The original JavaScript Date object to be converted.
+ * @param {string} zone - The time zone to be applied to the Luxon DateTime object.
+ * @returns {luxon.DateTime} The converted Luxon DateTime object with the specified time zone.
+ */
 const convertToLuxonDateTimeObject = (originalDateTime, zone) => {
 	return luxon.DateTime.fromObject(
 		{
@@ -34,6 +56,13 @@ const convertToLuxonDateTimeObject = (originalDateTime, zone) => {
 		{ zone }
 	);
 };
+
+/**
+ * Returns the image URL based on the provided title.
+ *
+ * @param {string} title - The title to check for specific keywords.
+ * @returns {string} The corresponding image URL based on the title.
+ */
 const getImageUrl = (title) => {
 	switch (true) {
 		case title.toLowerCase().includes("linkedin"):
@@ -64,6 +93,14 @@ const getImageUrl = (title) => {
 			return "generic-banner.jpg";
 	}
 };
+
+/**
+ * Fetches events from an RSS feed URL and parses them into a structured format.
+ *
+ * @param {string} rssFeedUrl - The URL of the RSS feed to fetch events from.
+ * @returns {Promise<Array<Object>|string>} A promise that resolves to an array of event objects or a string message if no items are found.
+ * @throws {Error} Throws an error if the HTTP request fails.
+ */
 const fetchEventsFromRssFeed = async (rssFeedUrl) => {
 	const response = await fetch(rssFeedUrl);
 	if (!response.ok) {
@@ -73,7 +110,7 @@ const fetchEventsFromRssFeed = async (rssFeedUrl) => {
 	const data = new DOMParser().parseFromString(str, "text/html");
 	const items = data.querySelectorAll("item");
 	if (items.length <= 0) {
-		return "No items found";
+		return [];
 	}
 	const result = [];
 	items.forEach((item) => {
@@ -120,6 +157,12 @@ const fetchEventsFromRssFeed = async (rssFeedUrl) => {
 	});
 	return result;
 };
+
+/**
+ * Creates and appends event cards to the events list.
+ *
+ * @param {Array} paginatedEvents - An array of event objects to be displayed.
+ */
 const createEventsCards = async (paginatedEvents) => {
 	const fragment = document.createDocumentFragment();
 	paginatedEvents.forEach((event) => {
@@ -127,6 +170,7 @@ const createEventsCards = async (paginatedEvents) => {
 		eventCard.classList.add("event-card");
 		const eventLink = document.createElement("a");
 		eventLink.href = event.link;
+		eventLink.target = "_blank";
 		eventCard.appendChild(eventLink);
 		const eventImage = document.createElement("img");
 		eventImage.classList.add("event-image");
@@ -161,12 +205,27 @@ const createEventsCards = async (paginatedEvents) => {
 	const eventsList = document.querySelector(".events-list");
 	eventsList.appendChild(fragment);
 };
+
+/**
+ * Display the range of events currently being shown and the total number of events.
+ *
+ * @param {Array} allEvents - An array containing all event objects.
+ * @param {number} startEventIndex - The index of the first event being displayed.
+ * @param {number} endEventIndex - The index of the last event being displayed.
+ */
 const showResultSummary = (allEvents, startEventIndex, endEventIndex) => {
 	const resultSummary = document.querySelector(".results-summary");
 	resultSummary.textContent = `Displaying ${startEventIndex + 1} to ${
 		endEventIndex < allEvents.length ? endEventIndex : allEvents.length
 	} of ${allEvents.length} results`;
 };
+
+/**
+ * Creates pagination buttons and handles pagination logic for displaying events.
+ *
+ * @param {Array} allEvents - An array of all event objects.
+ * @param {number} cardLimit - The number of events to display per page.
+ */
 const createPagination = (allEvents, cardLimit) => {
 	const paginationList = document.querySelector(".pagination-list");
 	const totalPages = Math.ceil(allEvents.length / cardLimit);
@@ -202,6 +261,11 @@ const createPagination = (allEvents, cardLimit) => {
 		});
 	}
 };
+
+/**
+ * Display the latest fetch time in the format "DD hh:mm a".
+ * The fetch time is converted to a Luxon DateTime object before formatting.
+ */
 const getlatestFetchTime = () => {
 	const lastUpdatedElement = document.querySelector(".rss-feed-last-updated");
 	const latestFetchTime = convertToLuxonDateTimeObject(new Date()).toFormat(
@@ -209,19 +273,66 @@ const getlatestFetchTime = () => {
 	);
 	lastUpdatedElement.textContent = `Last updated: ${latestFetchTime}`;
 };
-const renderEventCards = async (currentPage, rssFeedUrl, cardLimit) => {
-	const allEvents = await fetchEventsFromRssFeed(rssFeedUrl);
-	const startEventIndex = (currentPage - 1) * cardLimit;
-	const endEventIndex = startEventIndex + cardLimit;
-	const paginatedEvents = allEvents.slice(startEventIndex, endEventIndex);
-	createEventsCards(paginatedEvents);
-	showResultSummary(allEvents, startEventIndex, endEventIndex);
-	createPagination(allEvents, cardLimit);
-	getlatestFetchTime();
+
+/**
+ * Display a message indicating that no events are available in the results summary and sets the last updated timestamp to "N/A".
+ */
+const displayNoEventsMessage = () => {
+	const resultsSummary = document.querySelector(".results-summary");
+	resultsSummary.innerHTML = `No events available. 
+    Visit <strong><a class='anchor_underline' href='https://talent.deakin.edu.au/leap/events.html' target='_blank'>our events page</a></strong> to explore what else is on.`;
+	const lastUpdatedElement = document.querySelector(".rss-feed-last-updated");
+	lastUpdatedElement.textContent = "Last updated: N/A";
 };
+
+/**
+ * Displays an error message in the results summary and sets the last updated timestamp to "N/A".
+ *
+ * @param {string} error - The error message to be displayed.
+ */
+const displayErrorMessage = (error) => {
+	const resultsSummary = document.querySelector(".results-summary");
+	resultsSummary.textContent = `${error}`;
+	const lastUpdatedElement = document.querySelector(".rss-feed-last-updated");
+	lastUpdatedElement.textContent = "Last updated: N/A";
+};
+
+/**
+ * Renders the event section by fetching events from an RSS feed, displaying them, paginating them,
+ * and displaying a results summary based on the current page..
+ *
+ * @param {number} currentPage - The current page number to display.
+ * @param {string} rssFeedUrl - The URL of the RSS feed to fetch events from.
+ * @param {number} cardLimit - The number of event cards to display per page.
+ * @returns {Promise<void>} A promise that resolves when the event section has been rendered.
+ *
+ * @throws Will throw an error if there is an issue fetching events from the RSS feed.
+ */
+const renderEventSection = async (currentPage, rssFeedUrl, cardLimit) => {
+	try {
+		const allEvents = await fetchEventsFromRssFeed(rssFeedUrl);
+		if (allEvents.length === 0) {
+			displayNoEventsMessage();
+			return;
+		}
+		const startEventIndex = (currentPage - 1) * cardLimit;
+		const endEventIndex = startEventIndex + cardLimit;
+		const paginatedEvents = allEvents.slice(startEventIndex, endEventIndex);
+		createEventsCards(paginatedEvents);
+		showResultSummary(allEvents, startEventIndex, endEventIndex);
+		createPagination(allEvents, cardLimit);
+		getlatestFetchTime();
+	} catch (error) {
+		displayErrorMessage(error);
+	}
+};
+
+// Set initial current page number
 let currentPage = 1;
+// Get RSS feed URL and card limit from the DOM
 const rssFeedUrl = document.querySelector(".events-list").dataset.rssUrl;
 const cardLimit = Number.parseInt(
 	document.querySelector(".events-list").dataset.cardLimit
 );
-renderEventCards(currentPage, rssFeedUrl, cardLimit);
+// Render an event section
+renderEventSection(currentPage, rssFeedUrl, cardLimit);
